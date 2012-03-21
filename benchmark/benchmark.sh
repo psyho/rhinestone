17b441bb4798a3c101a278b3268c369001a2b340
Adam Pohorecki <adam.pohorecki@llp.pl>
2012-03-21 16:46:18 +0100

    Benchmark script

---
+++ benchmark/benchmark.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "without cache"
+rvm 1.9.3 gemset create rhinestone-benchmark
+time rvm 1.9.3@rhinestone-benchmark exec bundle install
+rvm --force gemset delete rhinestone-benchmark
+
+echo "with cache"
+rvm 1.9.3 gemset create rhinestone-benchmark
+time rvm 1.9.3@rhinestone-benchmark exec bundle install
+rvm --force gemset delete rhinestone-benchmark
+
+echo "no rubygems"
+rvm 1.9.3 gemset create rhinestone-benchmark
+time rvm 1.9.3@rhinestone-benchmark exec bundle install --gemfile=Gemfile.proxy
+rvm --force gemset delete rhinestone-benchmark
